c7ca21246e465ea0a0abf5abef236e4f1facf1ef
Max_Ust <maks_va@Inbox.ru>
2025-08-20 12:24:09 +0300

    madetest5

--- src/test/java/tests/SearchcodeinpageTest.java
+++ src/test/java/tests/SearchcodeinpageTest.java
@@ -19,17 +19,26 @@ public class SearchcodeinpageTest {
 
     @Test
     void fillFormTest() {
+        String codeExample = """
+                @ExtendWith({SoftAssertsExtension.class})
+                class Tests {
+                  @Test
+                  void test() {
+                    Configuration.assertionMode = SOFT;
+                    open("page.html");
+                
+                    $("#first").should(visible).click();
+                    $("#second").should(visible).click();
+                  }
+                }
+                """;
         open("/selenide/selenide");
         $("#wiki-tab").click();
         $$("li").findBy(text("Soft assertions")).shouldHave(text("Soft assertions"));
         $$("li").findBy(text("Soft assertions")).$("A").click();
-        $$(".markdown-heading").findBy(text("JUnit5")).sibling(0).$("[data-tooltip-direction=w]").click();
-        //$$(".markdown-heading").findBy(text("JUnit5")).sibling(0).shouldHave(text("ExtendWith"));
-        sleep(5001);
-
-
-
-
+        sleep(5000);
+        $$(".markdown-heading").findBy(text("JUnit5")).sibling(0).shouldBe(visible);
+        $$(".markdown-heading").findBy(text("JUnit5")).sibling(0).shouldHave(text(codeExample));
 
 
 
