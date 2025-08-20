3db4e61f17573982139a0debb6b7ba698c193654
Max_Ust <maks_va@Inbox.ru>
2025-08-20 03:10:12 +0300

    madetest

---
+++ src/test/java/tests/SearchInPage.java
@@ -0,0 +1,39 @@
+package tests;
+
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SearchInPage {
+
+    @BeforeAll
+    static void setupEnvironment() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://github.com/";
+        Configuration.pageLoadStrategy = "eager";
+    }
+
+    @Test
+    void fillFormTest() {
+        open("/selenide/selenide");
+        $("#wiki-tab").click();
+        $$("li").findBy(text("Soft assertions")).shouldHave(text("Soft assertions"));
+        $$("li").findBy(text("Soft assertions")).$("A").click();
+        $$(".markdown-heading").findBy(text("JUnit5")).sibling(0).shouldHave(text("ExtendWith"));
+        sleep(5000);
+
+
+
+
+
+
+
+
+
+    }
+
+}
